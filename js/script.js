740ba74ef7bc1b61fe171154415a137456e150f2
Angelo7x <abennici96@gmail.com>
2021-07-29 16:37:22 +0200

    fix js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//chiedere numero km
+var km = parseInt (prompt ("Inserisci i km da percorrere"));
+//chiedere etò passeggero
+var age = parseInt (prompt("Inserisci la tua età"));
+//Costo biglietto per km
+var pricekm 
+pricekm = 0.21
+var ticket = alert(pricekm * km)
+//Sconto 20%
+var ticket20
+if (age < 18) {
+    alert(ticket20 = (ticket - (ticket*20/100)))
+}
+console.log(ticket20)
+//sconto 40%
+// prezzo finale biglietto con eventuale sconto
